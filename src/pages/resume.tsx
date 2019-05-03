693b6800eb2bd039ea76f372e13839c9a4e858dc
Chappo <lukeggchapman@gmail.com>
2019-05-03 16:29:53 +1000

    Site now consists of only the resume PDF preview at full size

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
 import SEO from '../components/seo'
 import ResumePDF from '../components/ResumePDF'
 
 const ResumePage = () => {
   return (
-    <Layout>
+    <>
       <SEO title="Résumé" />
-      <h1>Résumé</h1>
       <ResumePDF />
-
-      <Link to="/">Go back to the homepage</Link>
-    </Layout>
+    </>
   )
 }
 
