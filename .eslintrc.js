afd73f13b2e03411aa85d079f13fbf7f40503199
Chappo <lukeggchapman@gmail.com>
2019-05-06 17:04:08 +1000

    Lint: Cleaned up linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-// Thanks to https://gist.github.com/1natsu172/a65a4b45faed2bd3fa74b24163e4256e
-
 const jsExtensions = ['.js', '.jsx']
 const tsExtensions = ['.ts', '.tsx']
 const allExtensions = jsExtensions.concat(tsExtensions)
@@ -19,19 +17,11 @@ module.exports = {
   extends: [
     'airbnb',
     'plugin:@typescript-eslint/recommended',
-    /**
-     * @bug https://github.com/benmosher/eslint-plugin-import/issues/1285
-     * import/named requires 'typescript-eslint-parser'
-     */
-    // 'plugin:import/typescript',
+    'plugin:import/typescript',
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
   ],
   settings: {
-    /**
-     * @bug https://github.com/benmosher/eslint-plugin-import/issues/1285
-     * import/named requires 'typescript-eslint-parser'
-     */
     'import/extensions': allExtensions,
     'import/parsers': {
       '@typescript-eslint/parser': tsExtensions,
@@ -47,29 +37,13 @@ module.exports = {
     /**
      * @description rules of eslint official
      */
-    /**
-     * @bug https://github.com/benmosher/eslint-plugin-import/issues/1282
-     * "import/named" temporary disable.
-     */
-    'import/named': 'off',
-    /**
-     * @bug?
-     * "import/export" temporary disable.
-     */
-    'import/export': 'off',
     'import/prefer-default-export': 'off', // Allow single Named-export
-    'no-unused-expressions': [
-      'warn',
-      {
-        allowShortCircuit: true,
-        allowTernary: true,
-      },
-    ], // https://eslint.org/docs/rules/no-unused-expressions
+
     /**
      * @description rules of @typescript-eslint
      */
-    '@typescript-eslint/prefer-interface': 'off', // also want to use "type"
     '@typescript-eslint/explicit-function-return-type': 'off', // annoying to force return type
+
     /**
      * @description rules of eslint-plugin-react
      */
@@ -79,11 +53,8 @@ module.exports = {
         extensions: ['.jsx', '.tsx'],
       },
     ], // also want to use with ".tsx"
-    'react/prop-types': 'off', // Is this incompatible with TS props type?
-    /**
-     * @description rules of eslint-plugin-react-hooks
-     */
-    'react-hooks/rules-of-hooks': 'error',
+    'react/prop-types': 'off', // we're relying on TS prop interfaces for this
+
     /**
      * @description rules of eslint-plugin-prettier
      */
@@ -99,15 +70,12 @@ module.exports = {
     {
       files: ['*.js'], // gatsby framework node files
       rules: {
-        '@typescript-eslint/no-var-requires': 'off', // gatsby still use nodes default var require
+        '@typescript-eslint/no-var-requires': 'off', // gatsby still uses nodes default var require
       },
     },
     {
       files: ['*.test.ts', '*.test.tsx'], // jest test files
       rules: {
-        /**
-         * Allow tests to import from devDependencies without error
-         */
         'import/no-extraneous-dependencies': [
           'error',
           {
@@ -115,11 +83,8 @@ module.exports = {
             optionalDependencies: false,
             peerDependencies: false,
           },
-        ],
-        /**
-         * Allow tests to use explicit any on mock values
-         */
-        '@typescript-eslint/no-explicit-any': 'off',
+        ], // Allow tests to import from devDependencies without error
+        '@typescript-eslint/no-explicit-any': 'off', // Allow tests to use explicit any on mock values
       },
     },
   ],
