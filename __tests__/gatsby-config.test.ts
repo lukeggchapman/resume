c026e5d69750be0bb91b820ed0b276d8a5c9e6ca
Chappo <lukeggchapman@gmail.com>
2019-06-14 02:30:52 +1000

    cleanup: removed unused global mock

--- __tests__/gatsby-config.test.ts
+++ __tests__/gatsby-config.test.ts
@@ -2,8 +2,6 @@ const gatsbyConfig = require('../gatsby-config')
 
 describe('Gatsby Config', () => {
   it('config has not changed', () => {
-    global.__dirname = 'some/path' // eslint-disable-line
-
     expect(gatsbyConfig).toMatchSnapshot()
   })
 })
