f58250cb0885d5032c4ef553871c2e25c84f40a4
Chappo <lukeggchapman@gmail.com>
2023-02-27 19:20:26 +1100

    chore: updated jest to the latest

--- jest.config.js
+++ jest.config.js
@@ -4,19 +4,20 @@ module.exports = {
   },
   testRegex: '(\\.|/)(test|spec)\\.([tj]sx?)$',
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.ts`,
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-mock.ts',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: [`node_modules`, `.cache`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'ts', 'tsx'],
+  testPathIgnorePatterns: ['node_modules', '.cache'],
+  transformIgnorePatterns: ['node_modules/'],
   globals: {
-    __PATH_PREFIX__: ``,
+    __PATH_PREFIX__: '',
   },
-  setupFiles: [`<rootDir>/jest/loaderShim.ts`],
+  setupFiles: ['<rootDir>/jest/loaderShim.ts'],
   setupFilesAfterEnv: ['<rootDir>/jest/setupEnzyme.ts'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  modulePaths: [`<rootDir>/src`],
+  modulePaths: ['<rootDir>/src'],
   coverageThreshold: {
     global: {
       branches: 80,
