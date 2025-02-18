84431524a29dbadeff73049b38e070ede1a0e9ae
Luke Chapman <luke.chapman@fireant.com.au>
2025-02-18 19:58:17 +1100

    fix: pdf bullet from locking up pdf render

--- src/utils/hastToPDF/components/PDFBulletList.tsx
+++ src/utils/hastToPDF/components/PDFBulletList.tsx
@@ -5,23 +5,13 @@ import { StyleSheet, View, Text } from '@react-pdf/renderer'
 const styles = StyleSheet.create({
   column: {
     flexDirection: 'column',
-    alignItems: 'flex-start',
-    width: 200,
   },
   row: {
     flexDirection: 'row',
-    alignItems: 'flex-start',
-    flexWrap: 'wrap',
-    flex: 1,
+    paddingBottom: 2,
   },
   bullet: {
     width: 10,
-  },
-  bulletText: {
-    flex: 1,
-  },
-  boldText: {
-    fontWeight: 'bold',
   },
 })
 
@@ -35,7 +25,7 @@ function PDFLi({ content }: PDFLiProps) {
       <View style={styles.bullet}>
         <Text>{'\u2022 '}</Text>
       </View>
-      <View style={styles.bulletText}>{content}</View>
+      <View>{content}</View>
     </View>
   )
 }
